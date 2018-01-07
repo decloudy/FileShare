b98e5f503446ce83163e0574c60aba9c93dd5bab
decloudy <31843403+decloudy@users.noreply.github.com>
2018-01-08 00:07:46 +0800

    v2.2
    更新messageDao和messageDaoImpl

---
+++ src/main/java/com/zust/FileShare/dao/impl/MessageDaoImpl.java
@@ -0,0 +1,20 @@
+package com.zust.FileShare.dao.impl;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.zust.FileShare.dao.MessageDao;
+import com.zust.FileShare.dao.UserDao;
+import com.zust.FileShare.entity.Message;
+import com.zust.FileShare.entity.User;
+
+@Repository
+public class MessageDaoImpl extends BaseDaoImpl<Message> implements MessageDao  {
+
+
+
+}
